46f057bd8210ac39f2dc559bdccb9f6611753f58
Rafael Victor <rafaelv.boscato@hotmail.com>
2022-01-21 14:48:23 -0300

    function find

--- funcs/find.js
+++ funcs/find.js
@@ -2,3 +2,7 @@
 const { passengers } = require("../data");
 
 // 1. Encontre o passageiro de nome: "Enzo"
+
+const findname = passengers.find(passengers => passengers.birthYear === 2004)
+
+console.log(findname)

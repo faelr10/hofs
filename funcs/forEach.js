4ae8967c131f223bec1fc87cb28f6f505035786d
Rafael Victor <rafaelv.boscato@hotmail.com>
2022-01-23 12:10:02 -0300

    function forEach

--- funcs/forEach.js
+++ funcs/forEach.js
@@ -1,3 +1,19 @@
 // FOREACH
-const { passengers } = require("../data");
+const { passengers,shopCart } = require("../data");
 // 1. Adicione a idade (age) para cada pessoa
+
+// passengers.forEach(passengers=>{
+//     passengers.age = 2022 - passengers.birthYear
+// })
+
+// console.table(passengers)
+
+shopCart.forEach(shopCart=>{
+
+    if(shopCart.quantity >= 2){  
+        shopCart.valueDescont = `${shopCart.unitPrice - ((shopCart.unitPrice * 20)/(100))}$` 
+    }
+    
+})
+
+console.table(shopCart)

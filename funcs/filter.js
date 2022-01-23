9b3248f4ce6e30bf7b203f1441a2e1206b41f715
Rafael Victor <rafaelv.boscato@hotmail.com>
2022-01-23 12:10:37 -0300

    add function filter

--- funcs/filter.js
+++ funcs/filter.js
@@ -1,18 +1,24 @@
 // FILTER
-const { childrenAge, mailList } = require("../data");
+const { childrenAge, mailList, passengers } = require("../data");
 
 // 1. Crianças entre 2 - 11 anos
 
 //const filterChildren = childrenAge.filter(
-    element => element==10 || element==15 
+    // element => element==10 || element==15 
 //)
 
 //console.table(filterChildren)
 
 // 2. Emails rocketseat.com.br
 
-const filteredMails = mailList.filter(itens => itens.includes("@"))
-console.log(filteredMails)
+// const filteredMails = mailList.filter(itens => itens.includes("@"))
+// console.log(filteredMails)
 
 // const filter = childrenAge.filter(age => age.includes("1"))
 // console.log(filter)
+
+const list = passengers.filter(
+    pass => pass.birthYear >= 2002
+)
+
+console.log(list)
